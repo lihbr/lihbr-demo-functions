80d1c5d0fb9c7eb46fa286a174c0f8e9ff856848
lihbr <lihbr@users.noreply.github.com>
2021-01-10 17:24:22 +0100

    ci(config): setup for ci

--- api/mem.js
+++ api/mem.js
@@ -1,8 +1,11 @@
+const source =
+  "https://github.com/lihbr/lihbr-demo-functions/blob/master/api/mem.js";
+
 let count = 0;
 
 module.exports = async (req, res) => {
   const ip = req.headers["x-real-ip"];
   count++;
 
-  return res.status(200).json({ ip, count });
+  return res.status(200).json({ ip, count, source });
 };
--- functions/mem.js
+++ functions/mem.js
@@ -1,3 +1,6 @@
+const source =
+  "https://github.com/lihbr/lihbr-demo-functions/blob/master/functions/mem.js";
+
 let count = 0;
 
 exports.handler = async (event, context) => {
@@ -7,6 +10,6 @@ exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     headers: { "content-type": "application/json" },
-    body: JSON.stringify({ ip, count })
+    body: JSON.stringify({ ip, count, source })
   };
 };
